22e3d442d1e8d9a86a3bbd98e93057f2369ea380
고범석 <kobumseouk@gobeomseog-ui-MacBookPro.local>
2025-06-16 19:41:15 +0900

    init

---
+++ Screeniary/ScreeniaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreeniaryApp.swift
+//  Screeniary
+//
+//  Created by 고범석 on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreeniaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
